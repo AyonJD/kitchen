42ea912692115372cf16248780370f4528c1394d
Ayon Jodder <83225232+AyonJD@users.noreply.github.com>
2023-06-17 11:33:13 +0600

    [Done] Recipe list

--- pages/demo-two/recipe.js
+++ pages/demo-two/recipe.js
@@ -1,24 +1,100 @@
-import { Container, Typography, useTheme } from "@mui/material";
+import { Box, Container, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography, useTheme } from "@mui/material";
+import { withStyles } from "@mui/styles";
+import { useState } from "react";
 import Page from "src/components/Page";
+import { MotionInView, varFadeInLeft, varFadeInRight } from "src/components/animate";
+import CustomCard from "src/components/card/CustomCard";
 import useSettings from "src/hooks/useSettings";
 import demoTwoSidebarConfig from "src/layouts/config/demoTowSidebarConfig";
 import DashboardLayout from "src/layouts/dashboard";
 
+// Custom styles for the hover effect
+const styles = {
+    hoverCell: {
+        '&:hover': {
+            backgroundColor: '#1ccaff',
+        },
+        borderRadius: '10px',
+    },
+};
 
-export default function Recipe() {
+function Recipe({ classes }) {
     const { themeStretch } = useSettings();
     const theme = useTheme();
-    const isLight = theme.palette.mode === 'light';
+    const [selectedIndex, setSelectedIndex] = useState(0);
+
+    const DATA_TITLE = ['Recipe One', 'Recipe Two', 'Recipe Three', 'Recipe Four', 'Recipe Five']
+    const DATA_IMAGE = ['/static/mock-images/demo-one-1.png', '/static/mock-images/demo-one-2.png', '/static/mock-images/demo-one-3.png', '/static/mock-images/demo-one-4.png', '/static/mock-images/demo-one-5.png']
+
+    const cardData = Array.from({ length: 5 }, (_, index) => ({
+        title: DATA_TITLE[index],
+        image: DATA_IMAGE[index],
+    }))
 
     return (
         <DashboardLayout sideBarConfig={demoTwoSidebarConfig}>
             <Page title="Kitchen | Recipe">
                 <Container maxWidth={themeStretch ? false : 'xl'}>
-                    <Typography variant="h3" component="h1" paragraph>
-                        Kitchen | Recipe Page
-                    </Typography>
+                    <Box sx={{
+                        display: 'flex', justifyContent: 'space-between', flexDirection: {
+                            xs: 'column',
+                            md: 'row'
+                        }
+                    }}>
+                        <MotionInView variants={varFadeInLeft} MotionInView sx={{
+                            width: {
+                                xs: '100%',
+                                sm: '80%',
+                                md: '30%'
+                            },
+                            margin: 'auto'
+                        }}>
+                            <CustomCard>
+                                <TableContainer>
+                                    <Table>
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell> Recipe List</TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {cardData.map((item, index) => (
+                                                <TableRow onClick={() => setSelectedIndex(index)} key={index}>
+                                                    <TableCell className={classes.hoverCell}> {item.title}</TableCell>
+                                                </TableRow>
+                                            ))}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                            </CustomCard>
+                        </MotionInView>
+
+                        <MotionInView sx={{
+                            width: {
+                                xs: '100%',
+                                sm: '80%',
+                                md: '50%'
+                            },
+                            margin: 'auto'
+                        }} variants={varFadeInRight}>
+                            <CustomCard sx={{
+                                marginTop: {
+                                    xs: 2,
+                                    md: 0
+                                }
+                            }}>
+                                <Typography variant="h6" sx={{ marginBottom: 2, textAlign: 'center' }}> Recipe Details</Typography>
+                                <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                                    <img src={cardData[selectedIndex].image} alt="recipe" />
+                                </Box>
+                                <Typography variant="small" sx={{ display: 'block', textAlign: 'center', fontSize: '15px' }}> {cardData[selectedIndex].title}</Typography>
+                            </CustomCard>
+                        </MotionInView>
+                    </Box>
                 </Container>
             </Page>
-        </DashboardLayout>
+        </DashboardLayout >
     )
 }
+
+export default withStyles(styles)(Recipe);
